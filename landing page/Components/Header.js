4edab1a3374bc51ff8ed9e76eee8f01facf6939b
paruljamwal <paruljamwal2000@gmail.com>
2022-02-24 12:03:04 +0530

    landing page
    along with header and footer

---
+++ landing page/Components/Header.js
@@ -0,0 +1,74 @@
+function Header(){
+    return `   <div id="box">
+    <div id="upbox">
+        <div id="logo">
+            <img id="logo1"
+                src="https://pisces.bbystatic.com/image2/BestBuy_US/Gallery/BestBuy_Logo_2020-190616.png"
+                alt="logo">
+        </div>
+        <div id="input">
+
+            <div id="line">
+
+                <button id="menu"> <i class="fa-solid fa-bars"></i>Menu</button>
+
+            </div>
+            <div id="inputbox">
+                <input id="inputbox1" type="text" placeholder="Search Best Buy "><i
+                    class="fa-solid fa-magnifying-glass"></i></input>
+            </div>
+        </div>
+        <div id="logo2">
+            <div id="logotext">
+                <p>North Anchorage</p>
+            </div>
+            <div id="logocart"><button id="logocartbtn"><i
+                        class="fa-solid fa-cart-shopping "></i>Cart</button>
+            </div>
+        </div>
+    </div>
+    <div id="lowbox">
+        <div id="lowbox1">
+            <div id="lowbox1a">Top Deals</div>
+            <div id="lowbox1b">Deal of the Day</div>
+            <div id="lowbox1c">Best Buy Totaltech</div>
+            <div id="lowbox1d">Credit Cards</div>
+            <div id="lowbox1e">Gift Cards</div>
+            <div id="lowbox1f">Gift Ideas</div>
+            <div id="lowbox1g">More<i class="fa-solid fa-chevron-down"></i></div>
+        </div>
+    
+        <div id="lowbox2">
+            <div id="account">Account<i class="fa-solid fa-chevron-down"></i></div>
+            <div id="recently">Recently Viewed<i class="fa-solid fa-chevron-down"></i></div>
+            <div id="order">order status<i class="fa-solid fa-chevron-down"></i></div>
+            <div id="saved">Saved Items<i class="fa-solid fa-chevron-down"></i></div>
+        </div>
+    </div>
+</div>`;
+}
+    
+    export default Header;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
